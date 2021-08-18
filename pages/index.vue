06f44b907401591f417e90679306c06f58a1bdc5
ok09ra <okudasota@gmail.com>
2021-08-18 21:48:09 +0900

    build for github page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,7 @@
 <template>
-  <Tutorial/>
+<div>
+ <h1>Hello, welcome to my profile site this is Sota Okuda Nice to meet you</h1>
+ <h3>testtesttest</h3>
+ 
+</div>
 </template>
-
-<script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({})
-</script>
