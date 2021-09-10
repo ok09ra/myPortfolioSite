7b7c448ffd74c2be32ae8f7ff0f19c0c1a3a28cb
ok09ra <okudasota@gmail.com>
2021-09-11 01:54:23 +0900

    大規模デザイン変更

--- pages/index.vue
+++ pages/index.vue
@@ -1,171 +1,149 @@
 <template>
 <div>
+  
   <div class="TopSection">
     <h1>Welcome</h1>
     <p class= "backgroundImg">
       <img src="/jpg/myIcon.jpg">
     </p>
   </div>
-  <div class="SelfIntroduction">
-    <p id="myIcon">
+
+  <div class="IntroductionSection">
+    <p id="ImageLeft">
       <img src="/jpg/myIcon.jpg">
     </p>
-    <div id="myProfile">
-      <h1 id="myName">okpn</h1>
-      <h3>守備範囲広めな生命体とソフトウェアのエンジニア&研究者(のたまご)です。</h3>
-      <h4>Speciality<br>合成生物学 理論生物学 バイオインフォマティクス 深層学習 秘密計算</h4>
+    <div id="MidBackgroundLeft"></div>
+    <h2 class="Myname">-okpn-</h2>
+    <h3 class="ThemePhrase">生命体とソフトウェアのエンジニアリングで<br>生命の叡智を再適用する。</h3>
+    <div class="MyPortfolioLink">
+      <a href="https://twitter.com/EnOkud"><img src="/svg/twitter.svg"></a>
+      <a href="https://www.facebook.com/sotaokudapn"><img src="/svg/facebook.svg"></a>
+      <a href="https://github.com/ok09ra"><img src="/svg/github.svg"></a>
+    </div>
+    <div class="CurrentPositions">
+      <h4>Current Position</h4>
+      <p>早稲田大学 先進理工学部 電気・情報生命工学科 B3<br>合成生物学研究室</p>
+      <p>SecHack 365 2021 思索駆動コース トレーニー</p>
+      <p>iGEM WASEDA IT TEAM</p>
+      <p>COUGER株式会社 QAエンジニア インターン</p>
+      <p>LITE株式会社 エンジニア</p>
+      <p>Beyond 2020 NEXT project 幹事長補佐</p>
+      <p>早稲田大学グローバル科学知融合研究所<br>SDGs教育カリキュラム作成ワーキンググループ<br>学生統括</p>
+    </div>
+  </div>
 
-      <div id="myPortfolioLink">
-        <a href="https://twitter.com/EnOkud">Twitter</a>
-        <a href="https://www.facebook.com/sotaokudapn">FaceBook</a>
-        <a href="https://github.com/ok09ra">Github</a>
+  <div class="EngineeringSection">
+    <p id="ImageRight">
+      <img src="/gif/coding.gif">
+    </p>
+    <div id="MidBackgroundRight"></div>
+    <h2 id="SectionTitleRight">Engineering</h2>
+    <div class="EngineeringSkills">
+
+      <div id="Skill">
+        <img  src="/svg/python.svg">
+        <h3>Python</h3>
+        <p id="SkillRate">★★★★★</p>
+        <p id="AddInfo">TensorFlow/Pytorch<br>Django/Flask<br>(業務利用2年)</p>
+      </div>
+
+      <div id="Skill">
+        <img  src="/svg/nodejs.svg">
+        <h3>Node.js</h3>
+        <p id="SkillRate">★★★</p>
+        <p id="AddInfo">webpack/babel/express<br>外部API,chatbot開発<br>(業務利用１年)</p>
+      </div>
+
+      <div id="Skill">
+        <img  src="/svg/unity.svg">
+        <h3>Unity</h3>
+        <p id="SkillRate">★</p>
+        <p id="AddInfo">ゲーム開発初心者<br>(業務利用)</p>
+      </div>
+
+      <div id="Skill">
+        <img  src="/svg/go.svg">
+        <h3>Go言語</h3>
+        <p id="SkillRate"></p>
+        <p id="AddInfo">バックエンド開発<br>スマートコントラクト開発<br>(業務利用これから)</p>
+      </div>
+      
+      <div id="Skill">
+        <img  src="/svg/Nginx.svg">
+        <h3>Nginx</h3>
+        <p id="SkillRate"></p>
+        <p id="AddInfo">バックエンド開発<br>(業務利用これから)</p>
+      </div>
+
+      <div id="Skill">
+      </div>
+
+      <div id="Skill">
+        <img  src="/svg/clang.svg">
+        <h3>c言語</h3>
+        <p id="SkillRate">★★</p>
+        <p id="AddInfo">ポインターの意味がわかる<br>簡単なアプリ実装<br>(趣味レベル)</p>
+      </div>
+
+      
+      <div id="Skill">
+        <img  src="/svg/raspberry-pi.svg">
+        <h3>Raspberry Pi</h3>
+        <p id="SkillRate">★★</p>
+        <p id="AddInfo">初めてのMy PC<br>電気回路をデジタル制御<br>(趣味レベル)</p>
+      </div>
+
+      <div id="Skill">
+        <img  src="/svg/julia.svg">
+        <h3>julia</h3>
+        <p id="SkillRate">★</p>
+        <p id="AddInfo">興味を持って<br>少しドキュメントを読んだ<br>(趣味歴2カ月)</p>
+      </div>
+
+      <div id="Skill">
+      </div>
+
+      <div id="Skill">
+        <img  src="/svg/tensorflow.svg">
+      </div>
+
+      <div id="Skill">
+        <img  src="/svg/django.svg">
+      </div>
+
+      <div id="Skill">
+        <img  src="/svg/vue.svg">
+      </div>
+
+      <div id="Skill">
+        <img  src="/svg/nuxt.svg">
       </div>
     </div>
   </div>
-  
-  <div class="ActivitySection">
-    <h1 class="Title">現在の活動</h1>
 
-    <div class="CardField">
-      <div class="card">
-        <img class="card-img" src="jpg/myIcon.jpg" alt="">
-        <div class="card-content">
-          <h1 class="card-title">早稲田大学 先進理工学部 <br>電気・情報生命工学科　B3</h1>
-          <p class="card-text">研究室では遺伝子回路を構築して新しい人工生命体を作る研究をしています。多細胞人工生物の創製を目標にしています。</p>
-        </div>
-        <div class="card-link">
-          <a href="http://webcreatorbox.com/about">About</a>
-          <a href="http://webcreatorbox.com/">Website</a>
-        </div>
-      </div>
-      <div class="card">
-        <img class="card-img" src="jpg/myIcon.jpg" alt="">
-        <div class="card-content">
-          <h1 class="card-title">iGEM WASEDA</h1>
-          <p class="card-text">2022年11月にフランス パリで開催される合成生物学の国際コンペティションiGEMに出場、成績を残すために現在活動しています。私はwetな実験やデータ解析を行いながら主にその中でもIT担当でwebサイトや各種コンテンツの作成を行っています。</p>
-        </div>
-        <div class="card-link">
-          <a href="http://webcreatorbox.com/about">About</a>
-          <a href="http://webcreatorbox.com/">Website</a>
-        </div>
-      </div>
-      
-      <div class="card">
-        <img class="card-img" src="jpg/myIcon.jpg" alt="">
-        <div class="card-content">
-          <h1 class="card-title">SecHack 365 2021<br>思索駆動コース</h1>
-          <p class="card-text">国立情報通信研究機構(NICT)のセキュリティイノベータ育成プログラムに参加し、遺伝情報の利活用に欠かせない情報管理スキームを構築しようと思っています。 </p>
-        </div>
-        <div class="card-link">
-          <a href="http://webcreatorbox.com/about">About</a>
-          <a href="http://webcreatorbox.com/">Website</a>
-        </div>
-      </div>
+  <div class="SocialSection">
+    <p id="ImageLeft">
+      <img src="/png/asahi-sdgs.png">
+    </p>
+    <div id="MidBackgroundLeft"></div>
+    <h2 id="SectionTitleLeft" style="font-size:6rem">Social Contributions</h2>
+    <h3>
+      早稲田大学グローバル科学知融合研究所で
+      <br>SDGs、持続可能な社会についての教材開発、小学生への出前講義により
+      <br>次世代人材育成の一端を担う活動に取り組んでいます。
+    </h3>
+    <h4>★我々の活動を毎日新聞様に取材いただき2021/8/17日付朝刊に掲載されました。<a href="https://mainichi.jp/articles/20210817/ddm/013/040/035000c">[こちら]</a></h4>
 
-      <div class="card">
-        <img class="card-img" src="jpg/myIcon.jpg" alt="">
-        <div class="card-content">
-          <h1 class="card-title">Beyond 2020 NEXT project<br>幹事長補佐</h1>
-          <p class="card-text">早稲田大学グローバル科学知融合研究所、早稲田大学アントレプレナーシップ人材育成プログラムWASEDA-EDGEのご協力を受け、起業家人材のコミュニティ形成のハブとなって活動しています。2021年8月17日毎日新聞の朝刊に我々がデロイトトーマツコンサルティング様と作成している小学生SDGs教育カリキュラムの実地デモの様子が掲載、取材を受けました。</p>
-        </div>
-        <div class="card-link">
-          <a href="http://webcreatorbox.com/about">About</a>
-          <a href="http://webcreatorbox.com/">Website</a>
-        </div>
-      </div>
-
-      <div class="card">
-        <img class="card-img" src="jpg/myIcon.jpg" alt="">
-        <div class="card-content">
-          <h1 class="card-title">COUGER株式会社<br>QAエンジニア インターン</h1>
-          <p class="card-text">人型AIエージェント(Virtual Human Agent)の開発で世界の先端を行く企業でプロダクトのQAを担当しています。最近はUIテストを全自動で行う社内システムをPythonで設計、開発、運用を行っています。</p>
-        </div>
-        <div class="card-link">
-          <a href="http://webcreatorbox.com/about">About</a>
-          <a href="http://webcreatorbox.com/">Website</a>
-        </div>
-      </div>
-
-      <div class="card">
-        <img class="card-img" src="jpg/myIcon.jpg" alt="">
-        <div class="card-content">
-          <h1 class="card-title">株式会社 LITE<br>エンジニア</h1>
-          <p class="card-text">スマホアプリ開発にフロント(Unity)からデータベース設計、開発(Golang, Nginx)まで縦横無尽に関わっています。(まだ初心者ですが、)</p>
-        </div>
-        <div class="card-link">
-          <a href="http://webcreatorbox.com/about">About</a>
-          <a href="http://webcreatorbox.com/">Website</a>
-        </div>
-      </div>
-      
-    </div>
   </div>
-  
-  <div class="ActivitySection">
-    <h1 class="Title">以前の活動</h1>
 
-    <div class="CardField">
-      <div class="card">
-        <img class="card-img" src="jpg/myIcon.jpg" alt="">
-        <div class="card-content">
-          <h1 class="card-title">日本政策金融公庫 高校生ビジネスプランコンテスト<br>ベスト100</h1>
-          <p class="card-text">高校2年時に友人2人とバドミントンの羽の再利用方法を模索、アクセサリーとして販売することをビジネスプランに落とし込み、全国ベスト100、埼玉県支部から表彰を頂きました。</p>
-        </div>
-        <div class="card-link">
-          <a href="http://webcreatorbox.com/about">About</a>
-          <a href="http://webcreatorbox.com/">Website</a>
-        </div>
-      </div>
-
-      <div class="card">
-        <img class="card-img" src="jpg/myIcon.jpg" alt="">
-        <div class="card-content">
-          <h1 class="card-title">ポピンズナーサリースクール<br>保育士サポータ</h1>
-          <p class="card-text">2019年2月から2カ月間、保育士さんのサポートで2-5才のお子さんのお世話をしていました。思ったよりも意思疎通ができること。日常で多用する単語や言葉を咀嚼して伝えることの難しさに驚きました。発達心理学に興味を持ちました。</p>
-        </div>
-        <div class="card-link">
-          <a href="http://webcreatorbox.com/about">About</a>
-          <a href="http://webcreatorbox.com/">Website</a>
-        </div>
-      </div>
-
-      <div class="card">
-        <img class="card-img" src="jpg/myIcon.jpg" alt="">
-        <div class="card-content">
-          <h1 class="card-title">株式会社 MAISON MARC<br>マーケティング部 インターン</h1>
-          <p class="card-text">大学1年生になる直前から1年間程、マーケティング部でクライアント様への提案資料の作成(主にwebサイト、コンテンツ系)、提案を行っていました。また半年ほどAI研究室を用意してもらい、自社ポートフォリオになるようなAIアプリケーションを複数実験実装していました。</p>
-        </div>
-        <div class="card-link">
-          <a href="http://webcreatorbox.com/about">About</a>
-          <a href="http://webcreatorbox.com/">Website</a>
-        </div>
-      </div>
-      
-      <div class="card">
-        <img class="card-img" src="jpg/myIcon.jpg" alt="">
-        <div class="card-content">
-          <h1 class="card-title">学生団体 L@ST-M@TE 学生統括</h1>
-          <p class="card-text">学生の「一歩踏み出す」を仲間とともにイベントを作り上げることで発生させる学生団体を一年間ほど運営していました。その際、外部向けイベント企画チームの統括としてSHIBUYA SCRAMBLE SQUARE14階のSHIBUYA QWSにてイベントを多数開催し、延べ参加人数は900人を達成しました。ここでチーム運営やリーダーシップへの興味を持ちました。</p>
-        </div>
-        <div class="card-link">
-          <a href="http://webcreatorbox.com/about">About</a>
-          <a href="http://webcreatorbox.com/">Website</a>
-        </div>
-      </div>
-
-      <div class="card">
-        <img class="card-img" src="jpg/myIcon.jpg" alt="">
-        <div class="card-content">
-          <h1 class="card-title">学生団体 WASEDA AI LAB<br>CUO(Update)</h1>
-          <p class="card-text">学生のAIリテラシーを上げるための学習コミュニティの運営を行っておりました。中では勉強会の企画からデータサイエンスコンペティション出場まで、様々な形でAIの知識、技能獲得を後押しする環境を整えました。</p>
-        </div>
-        <div class="card-link">
-          <a href="http://webcreatorbox.com/about">About</a>
-          <a href="http://webcreatorbox.com/">Website</a>
-        </div>
-      </div>
-
-    </div>
+  <div class="ResearchSection">
+    <p id="ImageRight">
+      <img src="/jpg/myIcon.jpg">
+    </p>
+    <div id="MidBackgroundRight"></div>
+    <h2 id="SectionTitleRight">Research</h2>
+    <h3>研究テーマ構築中　乞うご期待</h3>
+  </div>
 
   </div>
   
