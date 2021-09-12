7c41c29c926d6c878bd47c1cdb3b97e79b8f7f08
ok09ra <okudasota@gmail.com>
2021-09-12 21:39:13 +0900

    bugfix

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,11 +1,7 @@
 <template>
     <div class="Footer">
         <p>お気軽にご連絡ください</p>
-        <div id="MyPortfolioLink">
-            <a href="https://twitter.com/EnOkud"><img src="~assets/svg/twitter.svg"></a>
-            <a href="https://www.facebook.com/sotaokudapn"><img src="~assets/svg/facebook.svg"></a>
-            <a href="https://github.com/ok09ra"><img src="~assets/svg/github.svg"></a>
-        </div>
+
         <p>e-mail: okudasota@gmail.com</p>
     </div>
 </template>
