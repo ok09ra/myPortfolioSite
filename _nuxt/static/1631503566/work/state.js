4b01a1cb936f89bb88d11f6557b2c4da590f01c0
ok09ra <okudasota@gmail.com>
2021-09-13 12:26:29 +0900

    ebae0fd

---
+++ _nuxt/static/1631503566/work/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FmyPortfolioSite\u002F_nuxt\u002Fstatic\u002F1631503566",layout:"default",error:a,serverRendered:true,routePath:"\u002Fwork",config:{_app:{basePath:"\u002FmyPortfolioSite\u002F",assetsPath:"\u002FmyPortfolioSite\u002F_nuxt\u002F",cdnURL:a}}}}(null));
